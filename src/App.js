ef13df0a2c5776707b51b34bda7b2fb218033f25
Kenneth Elumba <kennethelumba@gmail.com>
2022-09-23 16:09:10 +0200

    add list of all beers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import {Routes, Route} from 'react-router-dom';
 import Header from './components/Header'
 import HomePage from './pages/HomePage';
+import BeersPage from './pages/BeersPage'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/beers" element={<BeersPage />} />
       </Routes>
     </div>
   );
---
+++ src/pages/BeersPage.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios';
+import { Link } from 'react-router-dom'
+
+export const BeersPage = () => {
+    const [beers, setBeers] = useState([]);
+    const API_URL = process.env.REACT_APP_BASE_URL;
+  
+    useEffect(() => {
+        axios.get(`https://ih-beers-api2.herokuapp.com/beers`)
+            .then(response => 
+                // console.log(response.data)
+                setBeers(response.data))
+            .catch(err => console.error(err))
+            // eslint-disable-next-line
+    }, [])
+
+    if (beers.length === 0){
+        return  <>
+                 <h3>List of Beers</h3>
+                 <p>Loading...</p>
+                </>
+    }
+
+    return (
+        <div>
+            <h1>List of Beers</h1>
+            {beers.map((beer) => (
+                <div key={beer._id}>
+                    <Link to={`/beers/${beer._id}`}>
+                        <img src={beer.image_url} alt="beer" style={{height: "400px"}}/>
+                        <h2>{beer.name}</h2>
+                        <h3>{beer.tagline}</h3>
+                        <h4>Created by: {beer.contributed_by}</h4>
+                    </Link>
+                </div>
+            ))}
+        </div>
+  )
+}
+
+export default BeersPage;
